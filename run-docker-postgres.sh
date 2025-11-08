7858556356358123802a005cff5652cfc2f4b1fd
haxdds <rahulchowdh97@gmail.com>
2025-11-08 09:09:39 -0500

    refactor: change name of postgres dev pod

--- run-docker-postgres.sh
+++ run-docker-postgres.sh
@@ -7,7 +7,7 @@
 # - `-d`: Run the container in "detached" mode (in the background).
 # - `postgres`: The Docker image to use (the official PostgreSQL image from Docker Hub).
 
-docker run --name postgres -e POSTGRES_PASSWORD=password -p 5432:5432 -d postgres
+docker run --name tickflow-postgres -e POSTGRES_PASSWORD=password -p 5432:5432 -d postgres
 
 # How to connect to this database from outside (e.g., from psql or a client library):
 # 
